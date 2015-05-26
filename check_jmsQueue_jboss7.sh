81b61a0361ce629286d831d1efef2dec6ce20899
rosevalfbj <rosevalfbj@gmail.com>
2015-05-26 10:06:16 -0300

    Check the message amount in the queue

---
+++ check_jmsQueue_jboss7.sh
@@ -0,0 +1,51 @@
+#/bin/bash
+while [ $# -gt 0 ]; do
+        case $1 in
+                "-w")
+                        shift
+                        WAR=${1}
+                ;;
+                "-c")
+                        shift
+                        CRI=${1}
+                ;;
+                "-q")
+                        shift
+                        QUEUE=${1}
+                ;;
+		"-h")
+			shift
+			SERVER=${1}
+		;;
+		"-p")
+			shift
+			SERVER_PORT=${1}
+		;;
+                *)
+                        echo "Use $0 -w [Warning] -c [Critical] -q [Queue] -h [Host] -p [Port]"
+                        exit 3
+                ;;
+        esac
+        shift
+done
+
+COMMAND_CLI="/subsystem=messaging/hornetq-server=default/jms-queue=${QUEUE}/:read-attribute(name=message-count)"
+COMMAND_JBOSS="/jboss/jboss-as-7.1.1/bin/jboss-cli.sh --connect controller=${SERVER}:${SERVER_PORT} --command=\"${COMMAND_CLI}\" "
+
+
+COMMAND=`sudo su - jboss -c "${COMMAND_JBOSS}"| awk '$1=="\"result\"" {print $3}'|sed 's/[^0-9]*//g' `
+RESULT=($COMMAND)
+
+if [ "${RESULT}" = "" ]; then
+    exit 2;
+else if [ ${RESULT} -gt ${CRI} ]; then
+        echo "Critical: Fila ${QUEUE} em acumulo qtde: ${RESULT} | Quantidade=${RESULT}"
+        exit 2;
+     else if [ ${RESULT} -gt ${WAR} ]; then
+            echo "Warning: Fila ${QUEUE} qtde: ${RESULT} | Quantidade=${RESULT}"
+            exit 1;
+          else echo "OK: Fila ${QUEUE} qtde: ${RESULT} | Quantidade=${RESULT}"
+               exit 0;
+          fi
+     fi
+fi
